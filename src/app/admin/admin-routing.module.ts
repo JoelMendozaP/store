1f6c7449007adc44bd1d193720ddad75c2d2ca84
JoelM <joel@Laptop.com>
2020-01-30 11:16:23 -0400

    Se completo la vista para el admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,18 +3,28 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
 
 const routes: Routes = [
- {
-  path: '',
-  component: NavComponent,
-  children: [
-    {
-      path: 'create',
-      component: ProductFormComponent
-    },
-  ]
-}
+  {
+    path: '',
+    component: NavComponent,
+    children: [
+      {
+        path: 'create',
+        component: ProductFormComponent
+      },
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'table',
+        component: TableComponent
+      },
+    ]
+  }
 ];
 
 @NgModule({
